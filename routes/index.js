e96fb9a7bdda33477c61260e373c1f530f9f9cef
Alejandro M <alejandromg@gmail.com>
2015-06-15 22:37:50 +0200

    Página de inicio y favicon
    Modificación de la página de inicio y se añade el favicon al proyecto

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+  	appTitle: 'Quiz',
+  	intro: 'Bienvenido a Quiz',
+  	descripcion: 'El portal donde podrá crear su propio juego de preguntas y respuestas.'
+  });
 });
 
 module.exports = router;
