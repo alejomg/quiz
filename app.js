eefe73acf6cb6de48cc63ce64e98f0aaaf05cde2
Alejandro M <alejandromg@gmail.com>
2015-06-17 00:31:42 +0200

    Marco de la aplicación
    Introducido un layout común a toda la aplicación. Se ha utilizado
    express-partials

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//añade vistas parciales y permite crear un layout común a la aplicación
+var partials = require('express-partials');
+
 var routes = require('./routes/index');
 
 var app = express();
@@ -12,6 +15,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
