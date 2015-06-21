b15f481d42379eb4d5a881e9a4017b9800e3c193
Alejandro M <alejandromg@gmail.com>
2015-06-22 00:52:54 +0200

    Despliegue DB en Heroku
    Se adapta la configuración de sequelize en el modelo, para que en local
    use SQLite y en producción (Heroku) utilice Postgres

--- models/models.js
+++ models/models.js
@@ -2,11 +2,32 @@
 
 var path = require("path");
 
+// Postgres DATABASE_URL=postgres://user:password@host:port/database
+// SQLite   DATABASE_URL=sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var protocol = (url[1] || null);
+var dialect  = (url[1] || null);
+var user     = (url[2] || null);
+var password = (url[3] || null);
+var host     = (url[4] || null);
+var port     = (url[5] || null);
+var DB_name  = (url[6] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 // cargar modelo ORM
 var Sequelize = require("sequelize");
 
-// usar DB SQLite
-var sequelize = new Sequelize(null, null, null, {dialect: "sqlite", storage: "quiz.sqlite"});
+// usar DB SQLite o Postgres
+//var sequelize = new Sequelize(null, null, null, {dialect: "sqlite", storage: "quiz.sqlite"});
+var sequelize = new Sequelize(DB_name, user, password,
+				{
+					protocol: protocol,
+					dialect: dialect,
+					host: host,
+					port: port,
+					storage: storage, // solo SQLite (.env)
+					omitNull: true    // solo Postgres
+				});
 
 // importar a la DB la definición de la tabla Quiz que está en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, "quiz"));
@@ -20,8 +41,8 @@ sequelize.sync().success(function() {
 		// si no hay ningún registro en la tabla, se añaden para inicializarla
 		if(count === 0) {
 			Quiz.create({
-				pregunta: "Capital de Inglaterra",
-				respuesta: "Londres"
+				pregunta: "Capital de Turquía",
+				respuesta: "Ankara"
 			})
 			.success(function(){console.log("DB started!!")});
 		}
